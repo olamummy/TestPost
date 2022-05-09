17a1a223862b499fd08b8408554922611bf648f8
Sulaiman Sulaiman <Sulaiman@lotusbetaanalytics.com>
2022-05-09 13:43:57 +0100

    Initial commit

---
+++ Data/MIMPOC1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MIMPOC1.Models;
+
+namespace MIMPOC1.Data
+{
+    public class MIMPOC1Context : DbContext
+    {
+        public MIMPOC1Context (DbContextOptions<MIMPOC1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MIMPOC1.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIMPOC1.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public string Mobile { get; set; }
+    }
+}
